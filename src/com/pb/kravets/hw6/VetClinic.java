7b8987dea735434865bbdeeb930a965f814dc950
Aleksandr Kravets <aleksandr.kravets@privatbank.ua>
2021-11-07 00:30:42 +0200

    init commit

--- src/com/pb/kravets/hw6/VetClinic.java
+++ src/com/pb/kravets/hw6/VetClinic.java
@@ -5,7 +5,10 @@ import java.lang.reflect.Constructor;
 
 public class VetClinic {
     public static void main(String[] args) throws Exception  {
-
+        Animal animal=new Animal("Собака", "Мясо", "Коврик");
+        animal.sleep();
+        animal.eat();
+        animal.makeNoise();
 
        Animal dog1 = new Dog("Лабрадор","Косточки", "Будка", 2,"Барбос");
        Animal dog2 =new Dog("Овчарка", "Собачий корм", "Вольер",5,"Рэкс");
@@ -13,6 +16,16 @@ public class VetClinic {
        Animal cat2 = new Cat("Сфинкс", "Котячий корм", "Чердак",3,"Васька");
        Animal horse1 = new Horse("Аукса", "Сено", "Конюшня", "Аукса", "Камелия");
        Animal horse2 = new Horse("Кински", "Лошадинный корм", "Сарай", "Кински", "Клеопатра");
+
+       dog1.eat();
+       dog1.sleep();
+       dog1.makeNoise();
+       cat1.eat();
+       cat1.sleep();
+       cat1.makeNoise();
+       horse1.eat();
+       horse1.sleep();
+       horse1.makeNoise();
 
        Animal[] arrayanimal=new Animal[]{dog1,dog2,cat1,cat2,horse1,horse2};
 
